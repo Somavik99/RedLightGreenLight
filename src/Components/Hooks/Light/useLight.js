b75840ab9cde54e9a9c890ebe54efca33df59856
Avik Roy <somavik99@gmail.com>
2023-09-19 13:33:05 +0530

    main

--- src/Components/Hooks/Light/useLight.js
+++ src/Components/Hooks/Light/useLight.js
@@ -1,7 +1,20 @@
+import { useState } from "react"
+import { ColorBg } from "./Color"
 
 
-const useLight = () => {
-  return {}
+
+function useLight () {
+    const [CurrentINdex,setCurrentIndex] = useState(0)
+    const [CountClick,setCountClick] = useState(0)
+    const [ColorState,setColorState] = useState(ColorBg)
+  
+
+const CLickCallBack = ()=>{
+
+}
+
+
+  return {CLickCallBack}
 }
 
 export default useLight
--- src/Components/Light/RLightGLight.jsx
+++ src/Components/Light/RLightGLight.jsx
@@ -1,15 +1,10 @@
-
-
 const RlightGLight = () => {
-  
-
   return (
     <div>
       <div className="">
-        <div className="w-[30vw] h-[30vw] border-[4px] border-gray-800 m-auto mt-24 rounded-2xl bg-gray-400">
-<div className="w-[8vw] h-[8vw] m-auto bg-black rounded-full">
-
-</div>
+        <div className="w-[30vw] md:w-[40vw] md:h-[40vw] h-[30vw] flex justify-center items-center flex-col  border-gray-800 m-auto mt-24 rounded-2xl bg-gray-200">
+          <div className="md:w-[15vw] w-[8vw] h-[8vw] md:h-[15vw] border-[2px] border-gray-800  rounded-full  mt-[12%] "></div>
+          <button className="m-auto w-[100px] h-[50px] border-[2px] border-gray-800 rounded-xl border-crimson bg-blue-gray-200 mt-[5%]">START</button>
         </div>
       </div>
     </div>
